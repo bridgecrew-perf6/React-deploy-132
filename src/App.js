f699f3b09e59277fcc4eb472f0a2de4cfa9b1bf2
mkazber <kazber97@wp.pl>
2022-01-31 20:10:41 +0100

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          EDIT CODEEEEEEEEEEEEEEEEE <code>src/App.js</code> and save to reload.
+          EDIT CODEEEeeeEEEEEEEEEEEEEE <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
